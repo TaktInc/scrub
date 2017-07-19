8bf2bd322e6fa19d1483a90422c724acc6c0e562
Doug Beardsley <mightybyte@gmail.com>
2017-07-19 09:34:58 -0400

    Initial version of Scrub and two included newtypes

---
+++ ChangeLog.md
@@ -0,0 +1,5 @@
+# Revision history for scrub
+
+## 0.1.0.0  -- 2017-07-19
+
+* First version. Released on an unsuspecting world.
---
+++ src/Scrub/Types.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE OverloadedStrings  #-}
+
+module Scrub.Types where
+
+------------------------------------------------------------------------------
+import           Data.Data
+import           Data.String
+import           Data.Text (Text)
+import qualified Data.Text as T
+import           Data.Typeable
+import           GHC.Generics
+------------------------------------------------------------------------------
+import           Scrub
+------------------------------------------------------------------------------
+
+
+------------------------------------------------------------------------------
+-- | Newtype wrapper for Text email addresses with an appropriate Scrub
+-- instance.
+newtype Email = Email { unEmail :: Text }
+  deriving (Eq,Ord,Show,Typeable,Data,Generic)
+
+instance IsString Email where
+  fromString = Email . T.pack
+
+instance Scrub Email where
+  scrub _ = "<email scrubbed>"
+
+------------------------------------------------------------------------------
+-- | Newtype wrapper for Text passwords with an appropriate Scrub instance.
+newtype Password = Password { unPassword :: Text }
+  deriving (Eq,Ord,Show,Typeable,Data,Generic)
+
+instance Scrub Password where
+  scrub _ = "<password scrubbed>"
+
+instance IsString Password where
+  fromString = Password . T.pack
